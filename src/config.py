7c9e47e444918d9167e1618967bce2d7b5353115
sanghun9634 <sanghun9634@gmail.com>
2025-05-13 23:33:44 +0900

    chore: fix env location

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import time
 from dotenv import load_dotenv
 
 # .env 파일 로드 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path=dotenv_path)
     print(f"Loaded .env file from: {dotenv_path}") 
